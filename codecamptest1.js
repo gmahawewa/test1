a84090aa0b370b5c8772a36dd5ea9566617de694
Gihan <133773671+gmahawewa@users.noreply.github.com>
2024-11-12 17:13:13 +0530

    Update codecamptest1.js

--- codecamptest1.js
+++ codecamptest1.js
@@ -1,5 +1,6 @@
-    test('#isNull, #isNotNull', function () {
-      assert.isNull.fail(null, 'This is an optional error description - e.g. null is null');
 
-      assert.isNotNull.fail(1, '1 is not null');
-    });
+    test('#isNull, #isNotNull', function() {
+        assert.isNull(null,'this is an optional error description - e.g. null is null');
+
+        assert.isNotNull(1, '1 is not null');
+      });
